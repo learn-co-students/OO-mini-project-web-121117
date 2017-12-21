4cd39db83faccad04ddd8228d5ee5e99b6337c1c
josephdlawson21 <josephdlawson21@gmail.com>
2017-12-21 18:26:22 -0500

    Done.

--- app/models/Ingredient.rb
+++ app/models/Ingredient.rb
@@ -1,1 +1,21 @@
+class Ingredient
+  attr_accessor :name
 
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.most_common_allergen
+    h = Allergen.all.map do |allergen_instance|
+      allergen_instance.ingredient
+    end
+    freq = h.inject(Hash.new(0)) { |hash,v| hash[v] += 1; hash }
+    h = h.max_by do |v|
+      freq[v]
+    end
+  end
+
+end
