1d2fb78075bcddd0637c1ae4d4a50df7288deb34
github username <github email address>
2019-02-25 11:33:29 -0800

    building up user

--- app/models/RecipeCard.rb
+++ app/models/RecipeCard.rb
@@ -1,4 +1,8 @@
-# A RecipeCard is the join between a user instance and a recipe instance. This is a has-many-through relationship. Build the following methods on the RecipeCard class:
+require 'date'
+
+# A RecipeCard is the join between a user instance and a recipe instance. This
+# is a has-many-through relationship. Build the following methods on the
+# RecipeCard class:
 
 # RecipeCard.all should return all of the RecipeCard instances
 # RecipeCard#date should return the date of the entry
@@ -6,8 +10,14 @@
 # RecipeCard#user should return the user to which the entry belongs
 # RecipeCard#recipe should return the recipe to which the entry belongs
 class RecipeCard
+    attr_reader :user, :rating, :recipe, :date
     @@all = []
-    def initialize()
+
+    def initialize(user, recipe, rating, date)
+        @user = user
+        @recipe = recipe
+        @rating = rating
+        @date = date
         @@all << self
     end
 
--- app/models/User.rb
+++ app/models/User.rb
@@ -1,17 +1,49 @@
-# User.all should return all of the user instances
-# User#recipes should return all of the recipes this user has recipe cards for
-# User#add_recipe_card should accept a recipe instance as an argument, as well as a date and rating, and create a new recipe card for this user and the given recipe
-# User#declare_allergen should accept an ingredient instance as an argument, and create a new allergen instance for this user and the given ingredient
-# User#allergens should return all of the ingredients this user is allergic to
-# User#top_three_recipes should return the top three highest rated recipes for this user.
-# User#most_recent_recipe should return the recipe most recently added to the user's cookbook.
 class User
     @@all = []
     def initialize()
         @@all << self
     end
 
+    # User.all should return all of the user instances
     def self.all
         @@all
+    end
+
+    # User#recipes should return all of the recipes this user has recipe cards for
+    def recipes
+        RecipeCard.select do |recipe_card|
+            recipe_card.user == self
+        end.map do |card|
+            card.recipe
+        end
+    end
+
+    # User#allergens should return all of the ingredients this user is allergic to
+    def allergens
+        Allergen.select do |allergen|
+            allergen.user == self
+        end.map do |allergen|
+            allergen.ingredient
+        end
+    end
+
+    # User#add_recipe_card should accept a recipe instance as an argument, as
+    # well as a date and rating, and create a new recipe card for this user and
+    # the given recipe
+    def add_recipe_card(recipe, date, rating)
+        RecipeCard.new(user, recipe, date, rating)
+    end
+
+    # User#declare_allergen should accept an ingredient instance as an argument, and create a new allergen instance for this user and the given ingredient
+    def declare_allergen(ingredient)
+        Allergen.new(user, ingredient)
+    end
+
+    # User#top_three_recipes should return the top three highest rated recipes for this user.
+    def top_three_recipes
+    end
+
+    # User#most_recent_recipe should return the recipe most recently added to the user's cookbook.
+    def most_recent_recipe
     end
 end
