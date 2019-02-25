84c0130194e05f4e313ca905254fc739bbaad7ea
github username <github email address>
2019-02-25 10:42:43 -0800

    adding comment descriptions for each clasS

--- app/models/Ingredient.rb
+++ app/models/Ingredient.rb
@@ -1,1 +1,4 @@
+# Build the following methods on the Ingredient class
 
+# Ingredient.all should return all of the ingredient instances
+# Ingredient.most_common_allergen should return the ingredient instance that the highest number of users are allergic to
